d883dfdfb3c9c6061147948baadd7a59e60ea379
bpm <3520124658@qq.com>
2025-08-15 11:44:14 +0800

    update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,10 @@ const rootPath = new URL('.', import.meta.url).pathname
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': rootPath + 'src',
